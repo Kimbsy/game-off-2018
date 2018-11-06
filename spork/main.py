93abcdb697d69b9e0f547f198ac9ab782ec2a050
Kimbsy <lordkimber@gmail.com>
2018-11-06 17:13:22 +0000

    adding comments

--- spork/main.py
+++ spork/main.py
@@ -1,13 +1,15 @@
 import pygame
 
+# Importing from sprites/base_sprites.py
 from sprites.base_sprites import ImageSprite
 
+# Initialise pygame stuff.
 pygame.init()
 clock = pygame.time.Clock()
 game_display = pygame.display.set_mode((750, 1000))
 pygame.display.set_caption('foo')
-pygame.display.update()
 
+# Global list of sprites to display.
 sprites = [
     ImageSprite(300, 225, 'w.png'),
     ImageSprite(490, 363, 'u.png'),
@@ -17,16 +19,29 @@ sprites = [
 ]
 
 def top_draggable_sprite_at_point(pos):
+    """Checks if there is a sprite in the global sprite
+    list under the mouse position which is draggable.
+
+    Reverses the sprite list so it finds sprites which
+    are 'on top' first.
+    """
     for sprite in reversed(sprites):
         if sprite.is_draggable and sprite.get_rect().collidepoint(pos):
             return sprite
 
 def gameloop():
+    """The main game loop.
+    """
+
+    # Want to move these elsewhere/design them away.
     done = False
     dragging = False
     dragged_sprite = None
 
+    # Want to refactor this body into seperate functions.
     while not done:
+
+        # Handle events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -49,14 +64,16 @@ def gameloop():
                     rel = event.rel
                     dragged_sprite.pos = (pos[0] + rel[0], pos[1] + rel[1])
         
+        # Display.
         game_display.fill((0, 0, 0))
 
         for sprite in sprites:
             sprite.display(game_display)
-                
+
         pygame.display.update()
     
 
+# Run the loop, quit when done.
 gameloop()
 pygame.quit()
 quit()
--- spork/sprites/base_sprites.py
+++ spork/sprites/base_sprites.py
@@ -2,22 +2,38 @@ import pygame
 import os
 
 class BaseSprite(object):
+    """Base sprite class.
+
+    Has x and y position, as well as skeleton function
+    for getting bounding rectangle.
+    """
 
     def __init__(self, x, y):
         self.pos = (x, y)
         self.is_draggable = False
 
     def display(self, game_display):
+        """Draw the sprite to the game_display.
+        """
         raise NotImplementedError('Please implement a display method')
 
     def get_size(self):
+        """Get the width and height of the sprite.
+        """
         raise NotImplementedError('Please implement a get_size method')
 
     def get_rect(self):
+        """Get the bounding rectangle for the sprite.
+        """
         size = self.get_size()
         return pygame.Rect(self.pos[0], self.pos[1], size[0], size[1])
 
 class ImageSprite(BaseSprite):
+    """Sprite which loads an image from ../data/ directory.
+
+    Currently this will load the image each time a sprite
+    is made, should cache images somewhere in future.
+    """
 
     def __init__(self, x, y, img_name):
         super(ImageSprite, self).__init__(x, y)
@@ -27,8 +43,13 @@ class ImageSprite(BaseSprite):
         self.is_draggable = True
 
     def display(self, game_display):
+        """Draw the bounding rectangle, then blit the
+        image to the screen.
+        """
         pygame.draw.rect(game_display, (0, 0, 255), self.get_rect(), 2)
         game_display.blit(self.image, self.pos)
 
     def get_size(self):
+        """Sprite size is based on the image.
+        """
         return self.image.get_size()
