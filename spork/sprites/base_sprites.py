47365e756b9790d1cc776a93014f33d7db9d1e1b
Kimbsy <lordkimber@gmail.com>
2018-11-06 16:52:19 +0000

    cleaning up, making dragging put sprites on top

--- spork/sprites/base_sprites.py
+++ spork/sprites/base_sprites.py
@@ -32,34 +32,3 @@ class ImageSprite(BaseSprite):
 
     def get_size(self):
         return self.image.get_size()
-
-class CompoundSprite(BaseSprite):
-
-    def __init__(self, x, y, children):
-        super(CompoundSprite, self).__init__(x, y)
-
-        self.children = children
-
-    def add(self, child):
-        child.pos = (child.pos[0] + self.pos[0], child.pos[1] + self.pos[1])
-        self.children.append(child)
-
-    def delete(self, child):
-        self.children.remove(child)
-
-    def display(self, game_display):
-        pygame.draw.rect(game_display, (0, 255, 255), self.get_rect(), 4)
-        for child in self.children:
-            child.display(game_display)
-
-    def get_size(self):
-        width = 0
-        height = 0
-
-        for child in self.children:
-            pos = (child.pos[0] - self.pos[0], child.pos[1] - self.pos[1])
-            size = child.get_size()
-            width = max(width, pos[0] + size[0])
-            height = max(height, pos[1] + size[1])
-        
-        return (width, height)
