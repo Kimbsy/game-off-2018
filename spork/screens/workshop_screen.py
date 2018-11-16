7c81a5c66a6e7d465eb5490934a404f20fbbbefe
BethFromAbove <bethcott@hotmail.co.uk>
2018-11-16 20:35:58 +0000

    Users can now delete items from the workshop screen, thumbnailsprite now allows users to specify size of sprite

--- spork/screens/workshop_screen.py
+++ spork/screens/workshop_screen.py
@@ -10,10 +10,15 @@ class ThumbnailSprite(ImageSprite):
     """Make thumbnails not draggable and small.
     """
 
-    def __init__(self, x, y, img_name):
+    def __init__(self, x, y, img_name, w, h):
+
+        self.w = w
+        self.h = h
+
         super(ThumbnailSprite, self).__init__(x, y, img_name)
 
         self.is_draggable = False
+
 
     def init_image(self):
         # Load the image from file and scale it to thumbnail size.
@@ -26,7 +31,9 @@ class ThumbnailSprite(ImageSprite):
 
         self.image.blit(loaded_img, (0, 0))
 
-        self.image = aspect_scale(self.image, (50, 50))
+        self.image = aspect_scale(self.image, (self.w, self.h))
+
+
 
 pygame.init()
 
@@ -34,31 +41,39 @@ def add_to_workbench(game_state, item_file):
 
     if not game_state.get('active_sprite1'):
         game_state.update({'active_sprite1': item_file})
-        all_sprites.add(ImageSprite(200, 300, item_file))
+        left_sprite.add(ThumbnailSprite(300, 300, item_file, 200, 200))
+        all_sprites.add(ButtonSprite(300, 250, 'X', remove_workbench_item, ['left']))
 
     elif not game_state.get('active_sprite2'):
         game_state.update({'active_sprite2': item_file})
-        all_sprites.add(ImageSprite(400, 300, item_file))
+        right_sprite.add(ThumbnailSprite(600, 300, item_file, 200, 200))
+        all_sprites.add(ButtonSprite(600, 250, 'X', remove_workbench_item, ['right']))
 
     else:
         print('You can only have 2 items')
 
     return game_state
 
-def remove_workbench_item(game_state):
+def remove_workbench_item(game_state, side):
 
-    game_state.update({'active_sprite1': None})
-    #game_state.update({'active_sprite2': None})
+    if side == 'left':
+        game_state.update({'active_sprite1': None})
+        left_sprite.empty()
+    elif side == 'right':
+        game_state.update({'active_sprite2': None})
+        right_sprite.empty()
 
     return game_state
 
 
 # Main group of sprites to display.
 all_sprites = pygame.sprite.Group()
+left_sprite = pygame.sprite.Group()
+right_sprite = pygame.sprite.Group()
 all_sprites.add(
-    ButtonSprite(50, 50, 'Splice!', switch_to_screen, ['splicer_screen']),
-    ButtonSprite(50, 100, 'QUIT', quit_game, []),
-    ButtonSprite(200, 300, 'X', remove_workbench_item, []),
+    ButtonSprite(300, 50, 'Splice!', switch_to_screen, ['splicer_screen']),
+    ButtonSprite(300, 100, 'QUIT', quit_game, []),
+    #ButtonSprite(200, 300, 'X', remove_workbench_item, []),
     #ButtonSprite(400, 300, 'X', remove_workbench_item, [right_sprite]),
 )
 
@@ -70,7 +85,7 @@ for item in items:
 
     item_file = os.getcwd() + '/data/pixel-components/' + item
 
-    all_sprites.add(ThumbnailSprite(x, y, item_file))
+    all_sprites.add(ThumbnailSprite(x, y, item_file, 50, 50))
 
     y += 25
     
@@ -123,6 +138,8 @@ def workshop_loop(game_state):
         # Display.
         game_surface.fill((0, 0, 0))
         all_sprites.draw(game_surface)
+        left_sprite.draw(game_surface)
+        right_sprite.draw(game_surface)
         pygame.display.update()
 
     return game_state
