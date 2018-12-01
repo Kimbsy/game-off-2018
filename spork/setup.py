cbc9ed714e5d914ec3b0807a66e68c59c8152afd
Kimbsy <lordkimber@gmail.com>
2018-12-01 19:05:11 +0000

    need to include font file

--- spork/setup.py
+++ spork/setup.py
@@ -5,6 +5,9 @@ executables = [cx_Freeze.Executable("spork.py")]
 cx_Freeze.setup(
     name="Spork",
     options={"build_exe": {"packages": ["pygame"],
-                           "include_files": [os.getcwd() + "/data"]}},
+                           "include_files": [
+                               os.getcwd() + "/data",
+                               "ARCADECLASSIC.TTF",
+                           ]}},
     executables=executables
 )
